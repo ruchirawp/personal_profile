b6bc11f1bcfc2a384f5ee212bd161c15a1243fc2
ruchi <ruchirawperera@gmail.com>
2023-01-14 00:12:02 -0500

    staggered animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,3 +253,21 @@
   new PureCounter();
 
 })()
+
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    // else{
+    //   entry.target.classList.remove('show');
+      
+    // }
+
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
